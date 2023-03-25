282b7006873f5fcf22be488126c1eaa21b8b75fd
Alexey 1981 <maximenko.av@rambler.ru>
2023-03-25 13:33:57 +0300

    Reformate2

--- src/main/java/ru/netology/sqr/SQRServise.java
+++ src/main/java/ru/netology/sqr/SQRServise.java
@@ -1,13 +1,14 @@
 package ru.netology.sqr;
+
 public class SQRServise {
     public int calcSQR(int firstBorder, int secondBorder) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
             int square = i * i;
             if ((firstBorder < square) & (square < secondBorder)) {
-                count ++;
+                count++;
             }
         }
-    return count;
+        return count;
     }
 }
