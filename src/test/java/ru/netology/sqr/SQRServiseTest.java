535e9586a4323218b7dd0a2f71dc4ed7cdc8d4d3
Alexey 1981 <maximenko.av@rambler.ru>
2023-03-25 11:39:33 +0300

    Reformate

--- src/test/java/ru/netology/sqr/SQRServiseTest.java
+++ src/test/java/ru/netology/sqr/SQRServiseTest.java
@@ -1,55 +1,16 @@
-//package ru.netology.sqr;
-//
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.Test;
-////import org.junit.jupiter.api.Test;
-////import ru.netology.sqr.SQRServise;
-//
-//public class SQRServiseTest {
-//    @Test
-//    public void avSQR() {
-//        SQRServise service = new SQRServise();
-//        int expected = 3;
-//        int actual = service.calcSQR(200, 300);
-//        Assertions.assertEquals(expected,actual);
-//    }
-//
-//
-//    @Test
-//    public void outSQR() {
-//        SQRServise service = new SQRServise();
-//        int expected = 0;
-//        int actual = service.calcSQR(2, 99);
-//        Assertions.assertEquals(expected,actual);
-//
-//    }
-//
-//    @Test
-//    public void underSQR() {
-//        SQRServise service = new SQRServise();
-//        int expected = 0;
-//        int actual = service.calcSQR(10_000, 20_000);
-//        Assertions.assertEquals(expected,actual);
-//    }
-//
-//}
 
 package ru.netology.sqr;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 public class SQRServiseTest {
     @ParameterizedTest
-//    @CsvSource({"3,200,300",
-//    "0,2,99",
-//    "0,10000,20000"})
+
     @CsvFileSource(files = "src/test/resources/sqr.csv")
     public void avSQR(int expected, int firstBorder, int secondBorder) {
         SQRServise service = new SQRServise();
-//        int expected = 3;
         int actual = service.calcSQR(firstBorder, secondBorder);
         Assertions.assertEquals(expected, actual);
     }
